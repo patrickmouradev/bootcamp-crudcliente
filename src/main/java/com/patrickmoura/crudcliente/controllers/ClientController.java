2975d0a116664ec01495157f7a70f30d6f6671c8
Patrick Moura <patrickmoura@gmail.com>
2021-05-17 16:57:55 -0300

    Controller/Exceptions/findAll and FindById

--- src/main/java/com/patrickmoura/crudcliente/controllers/ClientController.java
+++ src/main/java/com/patrickmoura/crudcliente/controllers/ClientController.java
@@ -7,14 +7,13 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.patrickmoura.crudcliente.dto.ClientDTO;
 import com.patrickmoura.crudcliente.services.ClientService;
-
-
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -33,6 +32,13 @@ public class ClientController {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction),orderBy);
 		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		return new ResponseEntity<>(list,HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById( @PathVariable Long id){
+			
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
